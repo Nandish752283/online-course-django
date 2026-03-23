9d3bf20840e392aed85468737576ac534b1693b5
Nandish752283 <104014663+Nandish752283@users.noreply.github.com>
2026-03-23 09:25:44 +0100

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Course, Lesson, Question, Choice, Submission
-from django.contrib.auth.models import User
+from .models import Course, Lesson, Question, Choice, Submission, Instructor, Learner
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -10,12 +9,19 @@ class QuestionInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'lesson', 'grade')
 
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+    list_display = ('title', 'course')
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
+admin.site.register(Instructor)
+admin.site.register(Learner)
